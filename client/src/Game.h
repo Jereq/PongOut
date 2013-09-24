5db2ba5a6ae153c7eab45686bea8e5ed14302dc0
Sebastian Larsson <sjereq@gmail.com>
2013-09-24 12:29:45 +0200

    Small unimportant project fixes.

--- client/src/Game.h
+++ client/src/Game.h
@@ -2,12 +2,18 @@
 
 #include "ICoreSystem.h"
 
+/**
+ * Main game class responsible for managing screens and the game loop.
+ */
 class Game
 {
 private:
 	ICoreSystem* system;
 
 public:
+	/**
+	 *
+	 */
 	Game(ICoreSystem* _system);
 
 	void run();
